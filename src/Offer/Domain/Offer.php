faf11397fc44df178528443ae5223c931d81f081
Busra Akkan <busrakkan@gmail.com>
2026-01-14 13:04:50 +0100

    feat: define core properties for Offer entity. Closed #14 #15

--- src/Offer/Domain/Offer.php
+++ src/Offer/Domain/Offer.php
@@ -4,6 +4,15 @@ declare(strict_types=1);
 
 namespace App\Offer\Domain;
 
+use DateTimeImmutable;
+
 final class Offer
 {
+    private string $id;
+    private string $title;
+    private string $providerName;
+    private string $utilityType;
+    private float $pricePerMonth;
+    private bool $isActive;
+    private DateTimeImmutable $createdAt;
 }
