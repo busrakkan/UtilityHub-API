45ac0808c1196c5ea94164450077b3c51b6c5c6c
Busra Akkan <busrakkan@gmail.com>
2026-01-15 14:10:48 +0100

    feat: implement OfferRepository with Doctrine ORM. Closed #27

---
+++ src/Offer/Infrastructure/Doctrine/OfferRepository.php
@@ -0,0 +1,38 @@
+<?php
+namespace App\Offer\Infrastructure\Doctrine;
+
+use App\Offer\Domain\Offer;
+use App\Offer\Domain\OfferRepositoryInterface;
+use Doctrine\ORM\EntityManagerInterface;
+
+class OfferRepository implements OfferRepositoryInterface
+{
+    private EntityManagerInterface $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    public function save(Offer $offer): void
+    {
+        $this->em->persist($offer);
+        $this->em->flush();
+    }
+
+    public function findById(string $id): ?Offer
+    {
+        return $this->em->getRepository(Offer::class)->find($id);
+    }
+
+    public function findAll(): array
+    {
+        return $this->em->getRepository(Offer::class)->findAll();
+    }
+
+    public function delete(Offer $offer): void
+    {
+        $this->em->remove($offer);
+        $this->em->flush();
+    }
+}
